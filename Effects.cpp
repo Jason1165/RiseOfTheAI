d817d1d400b68cac66c1f10890cc4681bb1a7034
Jason <Jason@VS>
2025-04-04 00:21:47 -0400

    header comment

--- Effects.cpp
+++ Effects.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Jason Lin
+* Assignment: Rise of the AI
+* Date due: 2025-04-05, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include "Effects.h"
 
 Effects::Effects(glm::mat4 projection_matrix, glm::mat4 view_matrix) :
--- LevelC.cpp
+++ LevelC.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Jason Lin
+* Assignment: Rise of the AI
+* Date due: 2025-04-05, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include "LevelC.h"
 #include "Utility.h"
 
